2f31918e60b77747aea04f469601d1f280073e10
Mitchell Luhm <mitchell.luhm@gmail.com>
2019-01-29 15:17:35 -0600

    Adding singly-linked list class

---
+++ Java/LinkedLists/src/SLinkedList.java
@@ -0,0 +1,14 @@
+
+public class SLinkedList {
+	
+	public SNode head;
+	
+	public SLinkedList() {
+		this.head = new SNode();
+	}
+	
+	public SLinkedList(int data) {
+		this.head = new SNode(data);
+	}
+
+}
